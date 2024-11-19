99a2848506c1aaaaacd5f4b1e437d60641633420
rogeriosenac <roge.pupo.toledo@gmail.com>
2024-11-19 11:56:06 -0300

    ajustes css responsividade

--- includes/header.php
+++ includes/header.php
@@ -6,7 +6,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Academia Acqua Vida</title>
     <!-- Ícone Font Awesome -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css"
+        integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
@@ -17,28 +19,31 @@
 
 <body>
     <!-- Cabeçalho e Navegação -->
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <div class="container-fluid nav_bar">
-            <a class="navbar-brand" href="#">
-                <img src="/Projetos_Git/Projeto-Acqua-Vida/assets/imagem/logo_acqua_vida.jpg" alt="Logotipo" class="img-fluid" style="max-height: 100px;">
-            </a>
-            <h1 class="texto">Somos uma equipe promovendo Saúde!</h1>
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav ms-auto">
-                    <li class="nav-item">
-                        <a class="nav-link" href="../index.php">Início</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="./pages/quem_somos.php">Sobre Nós</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="https://www.facebook.com/AcademiaAcquaVida/photos_by?locale=pt_BR" target="_blank">Fotos</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="https://www.facebook.com/AcademiaAcquaVida/videos?locale=pt_BR" target="_blank">Videos</a>
-                    </li>
-                </ul>
-            </div>
+    <div class="container-fluid">
+        <a class="piscina" href="#">
+            <img src="/Projetos_Git/Projeto-Acqua-Vida/assets/imagem/logo_acqua_vida.jpg" alt="Logotipo"
+                class="img-fluid">
+        </a>
+    </div>
+    <h1 class="texto">Somos uma equipe promovendo Saúde!</h1>
+    <nav class="navbar navbar-expand-lg">
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav mx-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="../index.php">Início</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="./pages/quem_somos.php">Sobre Nós</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="https://www.facebook.com/AcademiaAcquaVida/photos_by?locale=pt_BR"
+                        target="_blank">Fotos</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="https://www.facebook.com/AcademiaAcquaVida/videos?locale=pt_BR"
+                        target="_blank">Videos</a>
+                </li>
+            </ul>
         </div>
     </nav>
 
